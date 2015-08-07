17f5a690ff0ef61d2820ff80e4c88564fd3fa454
Suraj Masand <surajmasand@Surajs-iMac.local>
2015-08-07 10:57:38 -0400

    Signature method working, 3 sets of tests

--- KeyUtilsProject/KeyUtilsProject/KeyUtils.cs
+++ KeyUtilsProject/KeyUtilsProject/KeyUtils.cs
@@ -1,34 +1,40 @@
 ﻿using System;
 using System.IO;
 using System.Text;
+using System.Text.RegularExpressions;
+using System.Security.Cryptography;
 using System.Globalization;
+
 using NUnit.Framework;
+
 using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Digests;
 using Org.BouncyCastle.Utilities.IO.Pem;
+using Org.BouncyCastle.Utilities;
+using Org.BouncyCastle.Math;
+
 using BitCoinSharp;
 
 namespace KeyUtilsProject
 {
 	public class KeyUtils
 	{
+		private static string pemPattern = "-----BEGIN EC PRIVATE KEY-----\nMHQCA.*SuBBAAK\noUQDQ.*\n.*\n.*END EC PRIVATE KEY-----\n"; 
 
 		public static String generatePem()
 		{
+			// Create a new key
 			BitCoinSharp.EcKey keys = new BitCoinSharp.EcKey();
-			Console.WriteLine (keys.ToString());
+
 			StringWriter stringWriter = new StringWriter ();
 			PemWriter pemWriter = new PemWriter (stringWriter);
 
-	//		byte[] bArray = Encoding.UTF8.GetBytes (inputString);
-
 			const string DER1 = "30740201010420";
 
-
 			string privKey = (privKeyFromKeyPair (keys));
-			Console.WriteLine (privKey);
-			if (privKey.Length > 64 && privKey.Substring (0, 2) == "00") {
-				privKey = privKey.Substring (2);
-			}
+
+			// Make sure private key is in correct format
+			privKey = checkHas64 (privKey); 
 
 			const string DER2 = "a0070605";
 			const string DER3 = "2b8104000a";
@@ -39,8 +45,10 @@ namespace KeyUtilsProject
 
 			PemObject pemObj = new PemObject("EC PRIVATE KEY", hexToBytes(fullDER));
 			pemWriter.WriteObject(pemObj);
-			
-			return stringWriter.ToString ();
+
+			string pem = stringWriter.ToString ();
+
+			return pem;
 		}
 
 
@@ -61,43 +69,89 @@ namespace KeyUtilsProject
 			return privKey;
 		}
 
-		private static string createKeyPair()
+
+		public static string getSinFromPem (string pem)
 		{
-			BitCoinSharp.EcKey key = new BitCoinSharp.EcKey();
-			return key.ToString();
-			
-		}
-		
-		private static int getHexVal(char hex)
-		{
-			int val = (int)hex;
-			return val - (val < 58 ? 48 : (val < 97 ? 55 : 87));
-		}
-		
-		
-		private static byte[] hexToBytes(String hex) 
-		{
-			char[] hexArray = hex.ToCharArray();
-			
-			if (hex.Length % 2 == 1) {
-				throw new ArgumentException("Error: The binary key cannot have an odd number of digits");
-			}
-			
-			byte[] arr = new byte[hex.Length >> 1];
-			
-			for (int i = 0; i < hex.Length >> 1; ++i) {
-				arr[i] = (byte)((getHexVal(hexArray[i << 1]) << 4) + (getHexVal(hexArray[(i << 1) + 1])));
-			}
-			
-			return arr;
+			string[] keyInfo = keysFromPem (pem);
+			string hexPubKey = keyInfo [1];
+
+			// Step 1 and 2
+			// Hex to Bytes, SHA-256 and RIPEMD-160		
+			byte[] bytesPubKey1 = hexToBytes(hexPubKey);
+			byte[] bytesPubKey2 = Utils.Sha256Hash160(bytesPubKey1);
+			String shaAndRipe = Utils.BytesToHexString(bytesPubKey2);
+
+			// Step 3
+			// Add 0F02 to Hex of Step 2
+			String step3 = "0F02" + shaAndRipe;
+
+			// Step 4
+			// Hex to Bytes, Double SHA-256
+			byte[] bytesPubKey3 = hexToBytes(step3);
+			byte[] bytesPubKey4 = Utils.DoubleDigest(bytesPubKey3); //Utils.doubleDigest(bytesPubKey3);
+
+			// Step 5
+			// Substring of first 4 bytes (first 8 characters)
+			String step4Hex = Utils.BytesToHexString(bytesPubKey4);
+			String step5 = step4Hex.Substring(0, 8);
+
+			// Step 6
+			// Combine step 3 and step 5
+			String step6 = step3 + step5;
+
+			// Convert to base 58
+			byte[] step6bytes = hexToBytes(step6);
+			String sin = Base58.Encode(step6bytes);
+
+			return sin;
 		}
 
-		private static byte[] GetBytes(string str)
+		public static string signMsgWithPem (string msg, string pem)
 		{
-			byte[] bytes = new byte[str.Length * sizeof(char)];
-			System.Buffer.BlockCopy(str.ToCharArray(), 0, bytes, 0, bytes.Length);
-			return bytes;
+			if (msg == null || msg.Length == 0)
+				throw new ArgumentException("Message cannot be empty.");
+
+			string[] keyInfo = keysFromPem (pem);
+			string privKey = keyInfo [2];
+//			Console.WriteLine ("Uncomp PubKey: " + keyInfo[0]);
+
+			BigInteger privKeyNum = new BigInteger(privKey, 16);
+			BitCoinSharp.EcKey keys = new BitCoinSharp.EcKey (privKeyNum);
+
+			byte[] msgBytes = Encoding.UTF8.GetBytes (msg);
+
+			SHA256Managed hashstring = new SHA256Managed();
+			byte[] msgHash = hashstring.ComputeHash(msgBytes);
+
+			byte[] signedMsgBytes = keys.Sign (msgHash);
+			string signedMsg = Utils.BytesToHexString (signedMsgBytes);
+
+
+//			Sha256Digest shaDigest = new Sha256Digest ();
+//			shaDigest.BlockUpdate (msgBytes, 0, msgBytes.Length);
+//
+//			byte[] digestBytes = new byte[10];
+//			shaDigest.DoFinal (digestBytes, 3);
+//
+//			byte[] signedMsgBytes = keys.Sign (digestBytes);
+//			string signedMsg = Utils.BytesToHexString (signedMsgBytes);
+
+
+
+//			Sha256Hash msgHash = new Sha256Hash (msgBytes);
+//			int hashCode = msgHash.GetHashCode ();
+//			string hexLength = "X" + msg.Length;
+//			string hexString = hashCode.ToString (hexLength);
+//			byte[] signedMsgBytes = keys.Sign (hexToBytes(hexString));
+//			string signedMsg = Utils.BytesToHexString (signedMsgBytes);
+
+			return signedMsg;
 		}
+
+
+		// *************************************
+		// *          Private Methods          *
+		// *************************************
 
 		private static string uncompressedPubKeyFromKeyPair(BitCoinSharp.EcKey keys)
 		{
@@ -107,6 +161,8 @@ namespace KeyUtilsProject
 			int indexPubKeyEnd = keyString.LastIndexOf ("priv:") - 1; // to account for the space char before "priv:"
 
 			string pubKeyString = keyString.Substring (indexPubKeyStart, indexPubKeyEnd - indexPubKeyStart);
+			pubKeyString = checkHas64 (pubKeyString);
+
 			return pubKeyString;
 		}
 
@@ -117,13 +173,14 @@ namespace KeyUtilsProject
 			int indexPrivKeyStart = keyString.IndexOf ("priv:") + 5; // to account for the actual chars "priv:"
 
 			string privKeyString = keyString.Substring (indexPrivKeyStart);
+			privKeyString = checkHas64 (privKeyString);
 			return privKeyString;
 		}
 
 
 		private static string[] keysFromPem(string pem)
 		{
-
+			checkValidPEM (pem);
 			StringReader stringReader = new StringReader (pem);
 			PemReader pemReader = new PemReader (stringReader);
 
@@ -135,6 +192,9 @@ namespace KeyUtilsProject
 			string compPubKey = compPubKeyFromUncompPubKey (uncompPubKey);
 			string privKey = DERfromPEM.Substring (14, 64);
 
+			compPubKey = checkHas64 (compPubKey);
+			privKey = checkHas64 (privKey);
+
 			string[] keyInfo = {uncompPubKey.ToUpper (), compPubKey.ToUpper (), privKey.ToUpper ()};
 			return keyInfo;
 
@@ -145,11 +205,7 @@ namespace KeyUtilsProject
 			string xAndY = uncompPubKey; // removes leading "04"
 			string xVal = xAndY.Substring(0, 64);
 			int endOfY = int.Parse(xAndY.Substring (124), NumberStyles.AllowHexSpecifier);
-
-//			Console.WriteLine ("Full: " + xAndY);
-//			Console.WriteLine ("X: " + xVal);
-//			Console.WriteLine ("Y ending: " + endOfY);
-
+						
 			string prefix;
 			if (endOfY % 2 == 1) {
 				prefix = "03";
@@ -159,6 +215,53 @@ namespace KeyUtilsProject
 
 			string compPubKey = prefix + xVal;
 			return compPubKey;
+		}
+
+
+		private static void checkValidPEM(String pem) {
+			Boolean validPem = Regex.IsMatch (pem, pemPattern);
+			if (!validPem) {
+				throw new ArgumentException("PEM is not in a valid format.");
+			}
+
+		}
+
+		private static string checkHas64(String str) {
+			string str2 = str;
+			if (str2.Length % 2 == 1 && str2.Length < 64)
+				str2 = "0" + str2;
+			
+			while (str2.Length > 64 && str2.Substring (0, 2) == "00") 
+				str2 = str2.Substring (2);
+			
+			while (str2.Length < 64)
+				str2 = "00" + str2;
+
+			return str2;
+		}
+
+		private static int getHexVal(char hex)
+		{
+			int val = (int)hex;
+			return val - (val < 58 ? 48 : (val < 97 ? 55 : 87));
+		}
+
+
+		private static byte[] hexToBytes(String hex) 
+		{
+			char[] hexArray = hex.ToCharArray();
+
+			if (hex.Length % 2 == 1) {
+				throw new ArgumentException("Error: The binary key cannot have an odd number of digits");
+			}
+
+			byte[] arr = new byte[hex.Length >> 1];
+
+			for (int i = 0; i < hex.Length >> 1; ++i) {
+				arr[i] = (byte)((getHexVal(hexArray[i << 1]) << 4) + (getHexVal(hexArray[(i << 1) + 1])));
+			}
+
+			return arr;
 		}
 
 		
--- KeyUtilsProject/KeyUtilsProject/KeyUtilsTest.cs
+++ KeyUtilsProject/KeyUtilsProject/KeyUtilsTest.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Text;
 using System.Text.RegularExpressions;
+using System.Security.Cryptography;
 using NUnit.Framework;
+using BitCoinSharp;
 
 namespace KeyUtilsProject
 {
@@ -16,28 +19,54 @@ namespace KeyUtilsProject
 		static string pem1 = "-----BEGIN EC PRIVATE KEY-----\nMHQCAQEEICg7E4NN53YkaWuAwpoqjfAofjzKI7Jq1f532dX+0O6QoAcGBSuBBAAK\noUQDQgAEjZcNa6Kdz6GQwXcUD9iJ+t1tJZCx7hpqBuJV2/IrQBfue8jh8H7Q/4vX\nfAArmNMaGotTpjdnymWlMfszzXJhlw==\n-----END EC PRIVATE KEY-----\n";
 		static string pubKeyCompressed1 = "038D970D6BA29DCFA190C177140FD889FADD6D2590B1EE1A6A06E255DBF22B4017";
 		static string privateKey1 = "283B13834DE77624696B80C29A2A8DF0287E3CCA23B26AD5FE77D9D5FED0EE90";
-//		static string sin1 = "TeyN4LPrXiG5t2yuSamKqP3ynVk3F52iHrX";
-//		static string msg1 = "This is a test message.";
-//
-//		static string pem2 = "-----BEGIN EC PRIVATE KEY-----\nMHQCAQEEINMwkuB8YAtSTJzUNals8F2lXxsLIncL3rOc8dqRdps8oAcGBSuBBAAK\noUQDQgAEn0OPHdZ0hx+tLRobqDMbC5U12k+BAzynN/wMjzG3axbkgNIFGLim30pf\nh1Lvp4eFVHUydkbP250fTOrJ4zo7RQ==\n-----END EC PRIVATE KEY-----\n";
-//		static string pubKeyCompressed2 = "039F438F1DD674871FAD2D1A1BA8331B0B9535DA4F81033CA737FC0C8F31B76B16";
-//		static string privateKey2 = "D33092E07C600B524C9CD435A96CF05DA55F1B0B22770BDEB39CF1DA91769B3C";
-//		static string sin2 = "TfFW1ePJ5q6EZKBsYUrMGQEbb6z4fBmx6BW";
-//		static string msg2 = "Testing by using this message.";
+		static string sin1 = "TeyN4LPrXiG5t2yuSamKqP3ynVk3F52iHrX";
+		static string msg1 = "Test message 1";
+
+		static string pem2 = "-----BEGIN EC PRIVATE KEY-----\nMHQCAQEEINMwkuB8YAtSTJzUNals8F2lXxsLIncL3rOc8dqRdps8oAcGBSuBBAAK\noUQDQgAEn0OPHdZ0hx+tLRobqDMbC5U12k+BAzynN/wMjzG3axbkgNIFGLim30pf\nh1Lvp4eFVHUydkbP250fTOrJ4zo7RQ==\n-----END EC PRIVATE KEY-----\n";
+		static string pubKeyCompressed2 = "039F438F1DD674871FAD2D1A1BA8331B0B9535DA4F81033CA737FC0C8F31B76B16";
+		static string privateKey2 = "D33092E07C600B524C9CD435A96CF05DA55F1B0B22770BDEB39CF1DA91769B3C";
+		static string sin2 = "TfFW1ePJ5q6EZKBsYUrMGQEbb6z4fBmx6BW";
+		static string msg2 = "Test";
+
+		static string pem3 = "-----BEGIN EC PRIVATE KEY-----\nMHQCAQEEIGxJZ/H8cJ6rTejjSL/jg+bmvV6P1bh+oQyElxQfztbqoAcGBSuBBAAK\noUQDQgAEAJiwjbqNtzFoL1HvdRRWQgzEDMwN08PJC2bdqTmGHmZ0FfX5t+pOy4Ai\nOuluV4VbIFWZ64onGHXu0A7ejWY/jg==\n-----END EC PRIVATE KEY-----\n";
+		static string pubKeyCompressed3 = "020098B08DBA8DB731682F51EF751456420CC40CCC0DD3C3C90B66DDA939861E66";
+		static string privateKey3 = "6C4967F1FC709EAB4DE8E348BFE383E6E6BD5E8FD5B87EA10C8497141FCED6EA";
+		static string sin3 = "Tf3goZsaKmYcD5YWGv2bz2yifbrM3ogkbMd";
+		static string msg3 = "This is test message number 3";
+
 
 		public static void Main() 
 		{
-			testGeneratePem();
+			testGeneratePem ();
 			testCompressedKeyFromPem ();
 			testPrivateKeyFromPem ();
+			testSinFromPem ();
+			testSignMessageWithPem ();
+
+			testGeneratePem2 ();
+			testCompressedKeyFromPem2 ();
+			testPrivateKeyFromPem2 ();
+			testSinFromPem2 ();
+			testSignMessageWithPem2 ();
+
+			testGeneratePem3 ();
+			testCompressedKeyFromPem3 ();
+			testPrivateKeyFromPem3 ();
+			testSinFromPem3 ();
+			testSignMessageWithPem3 ();
+
 		}
 
+
+		// ********************************
+		// *       1st set of tests       *
+		// ********************************
 
 		[Test]
 		public static void testGeneratePem()
 		{
 			string pemTest = KeyUtils.generatePem ();
-			Console.WriteLine ("Generated Pem: \n" + pemTest);
+			Console.WriteLine ("Generated Pem 1: \n" + pemTest);
 			Assert.That(Regex.IsMatch(pemTest, pemPattern));
 		}
 
@@ -56,6 +85,170 @@ namespace KeyUtilsProject
 			Console.WriteLine ("Private Key 1: \n" + privKey);
 			Assert.AreEqual (privateKey1, privKey);
 		}
+
+		[Test]
+		public static void testSinFromPem()
+		{
+			string sinTest = KeyUtils.getSinFromPem (pem1);
+			Console.WriteLine ("Sin 1: \n" + sinTest);
+			Assert.AreEqual (sin1, sinTest);
+
+		}
+
+		[Test]
+		public static void testSignMessageWithPem()
+		{
+			string signedMsg = KeyUtils.signMsgWithPem (msg1, pem1);
+			Console.WriteLine ("Signature 1: \n" + signedMsg);
+
+			byte[] msgBytes = Encoding.UTF8.GetBytes (msg1);
+			SHA256Managed hashstring = new SHA256Managed();
+			byte[] msgHash = hashstring.ComputeHash(msgBytes);
+
+			byte[] signedMsgBytes = hexToBytes (signedMsg);
+			byte[] pubKeyBytes = hexToBytes (KeyUtils.getCompressedPubKeyFromPem (pem1));
+
+			Assert.That(BitCoinSharp.EcKey.Verify (msgHash, signedMsgBytes, pubKeyBytes));
+		}
+
+
+		// ********************************
+		// *       2nd set of tests       *
+		// ********************************
+
+		[Test]
+		public static void testGeneratePem2()
+		{
+			string pemTest2 = KeyUtils.generatePem ();
+			Console.WriteLine ("Generated Pem 2: \n" + pemTest2);
+			Assert.That(Regex.IsMatch(pemTest2, pemPattern));
+		}
+
+		[Test]
+		public static void testCompressedKeyFromPem2()
+		{
+			string compPubKey2 = KeyUtils.getCompressedPubKeyFromPem (pem2);
+			Console.WriteLine ("Comp Pub Key 2: \n" + compPubKey2);
+			Assert.AreEqual (pubKeyCompressed2, compPubKey2);
+		}
+
+		[Test]
+		public static void testPrivateKeyFromPem2()
+		{
+			string privKey2 = KeyUtils.getPrivKeyFromPem (pem2);
+			Console.WriteLine ("Private Key 2: \n" + privKey2);
+			Assert.AreEqual (privateKey2, privKey2);
+		}
+
+		[Test]
+		public static void testSinFromPem2()
+		{
+			string sinTest2 = KeyUtils.getSinFromPem (pem2);
+			Console.WriteLine ("Sin 2: \n" + sinTest2);
+			Assert.AreEqual (sin2, sinTest2);
+
+		}
+
+		[Test]
+		public static void testSignMessageWithPem2()
+		{
+			string signedMsg2 = KeyUtils.signMsgWithPem (msg2, pem2);
+			Console.WriteLine ("Signature 2: \n" + signedMsg2);
+
+			byte[] msgBytes2 = Encoding.UTF8.GetBytes (msg2);
+			SHA256Managed hashstring2 = new SHA256Managed();
+			byte[] msgHash2 = hashstring2.ComputeHash(msgBytes2);
+
+			byte[] signedMsgBytes2 = hexToBytes (signedMsg2);
+			byte[] pubKeyBytes2 = hexToBytes (KeyUtils.getCompressedPubKeyFromPem (pem2));
+
+			Assert.That(BitCoinSharp.EcKey.Verify (msgHash2, signedMsgBytes2, pubKeyBytes2));
+		}
+
+
+		// ********************************
+		// *       3rd set of tests       *
+		// ********************************
+
+		[Test]
+		public static void testGeneratePem3()
+		{
+			string pemTest3 = KeyUtils.generatePem ();
+			Console.WriteLine ("Generated Pem 3: \n" + pemTest3);
+			Assert.That(Regex.IsMatch(pemTest3, pemPattern));
+		}
+
+		[Test]
+		public static void testCompressedKeyFromPem3()
+		{
+			string compPubKey3 = KeyUtils.getCompressedPubKeyFromPem (pem3);
+			Console.WriteLine ("Comp Pub Key 3: \n" + compPubKey3);
+			Assert.AreEqual (pubKeyCompressed3, compPubKey3);
+		}
+
+		[Test]
+		public static void testPrivateKeyFromPem3()
+		{
+			string privKey3 = KeyUtils.getPrivKeyFromPem (pem3);
+			Console.WriteLine ("Private Key 3: \n" + privKey3);
+			Assert.AreEqual (privateKey3, privKey3);
+		}
+
+		[Test]
+		public static void testSinFromPem3()
+		{
+			string sinTest3 = KeyUtils.getSinFromPem (pem3);
+			Console.WriteLine ("Sin 3: \n" + sin3);
+			Assert.AreEqual (sin3, sinTest3);
+
+		}
+
+		[Test]
+		public static void testSignMessageWithPem3()
+		{
+			string signedMsg3 = KeyUtils.signMsgWithPem (msg3, pem3);
+			Console.WriteLine ("Signature 3: \n" + signedMsg3);
+
+			byte[] msgBytes3 = Encoding.UTF8.GetBytes (msg3);
+			SHA256Managed hashstring3 = new SHA256Managed();
+			byte[] msgHash3 = hashstring3.ComputeHash(msgBytes3);
+
+			byte[] signedMsgBytes3 = hexToBytes (signedMsg3);
+			byte[] pubKeyBytes3 = hexToBytes (KeyUtils.getCompressedPubKeyFromPem (pem3));
+
+			Assert.That(BitCoinSharp.EcKey.Verify (msgHash3, signedMsgBytes3, pubKeyBytes3));
+		}
+
+
+
+		// ****************************************************************
+		// *      Private Methods to convert hex string to byte array      *
+		// ****************************************************************
+
+		private static int getHexVal(char hex)
+		{
+			int val = (int)hex;
+			return val - (val < 58 ? 48 : (val < 97 ? 55 : 87));
+		}
+
+
+		private static byte[] hexToBytes(String hex) 
+		{
+			char[] hexArray = hex.ToCharArray();
+
+			if (hex.Length % 2 == 1) {
+				throw new ArgumentException("Error: The binary key cannot have an odd number of digits");
+			}
+
+			byte[] arr = new byte[hex.Length >> 1];
+
+			for (int i = 0; i < hex.Length >> 1; ++i) {
+				arr[i] = (byte)((getHexVal(hexArray[i << 1]) << 4) + (getHexVal(hexArray[(i << 1) + 1])));
+			}
+
+			return arr;
+		}
+
 
 	}
 }
